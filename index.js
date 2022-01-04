12280c5e3cda2030720cab6fb111c4f150a2d29a
Chris Yuh <50548791+christopheryuh@users.noreply.github.com>
2022-01-04 09:42:11 -0500

    added ability for button to be pressed with enter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 oldin = "";
+
 function clicked(){
   input = document.getElementById('input');
   num = document.getElementById('num');
@@ -15,3 +16,13 @@ function clicked(){
   }
   oldin = input.value;
 }
+
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    clicked();
+  }
+});
